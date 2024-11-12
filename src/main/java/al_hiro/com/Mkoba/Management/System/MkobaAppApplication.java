b5e832c7e268908f2516c0a937a26d04dfb7fd4b
Hassan Ngoma <munyiseke@gmail.com>
2024-11-12 16:04:10 +0300

    Initial Commit

---
+++ src/main/java/al_hiro/com/Mkoba/Management/System/MkobaAppApplication.java
@@ -0,0 +1,13 @@
+package al_hiro.com.Mkoba.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkobaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkobaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al_hiro/com/Mkoba/Management/System/MkobaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package al_hiro.com.Mkoba.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkobaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
